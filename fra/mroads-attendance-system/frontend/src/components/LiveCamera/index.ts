c970b66666fdceb1d5e8ca47f4ae2a7d08dfdc13
manikantareddy-07 <bharathvamsisatti@gmail.com>
2026-03-26 15:15:45 +0530

    Cleaned repo and removed unnecessary files

--- fra/mroads-attendance-system/frontend/src/components/LiveCamera/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as LiveCamera } from './LiveCamera';
-export type { LiveCameraRef } from './LiveCamera';
-export { MultiCameraManager } from './MultiCameraManager';
-export { CameraGridView } from './CameraGridView';
-export { default as ErrorBoundary } from './ErrorBoundary';
-
--- fra/mroads-attendance-system/frontend/src/components/LiveTransactions/index.ts
+++
@@ -1,2 +0,0 @@
-export { LiveTransactions } from './liveTransactions';
-
